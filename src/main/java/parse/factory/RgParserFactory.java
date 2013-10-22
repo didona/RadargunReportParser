f342d0f964eb0493d114803a98b2dfb462e8e9b6
Diego Didona <didona@gsd.inesc-id.pt>
2013-10-22 15:36:10 +0100

    Wiring pb

---
+++ src/main/java/parse/factory/RgParserFactory.java
@@ -0,0 +1,20 @@
+package parse.factory;
+
+import parse.radargun.Ispn5_2CsvParser;
+import parse.radargun.Ispn5_2CsvParser_PB;
+
+/**
+ * // TODO: Document this
+ *
+ * @author diego
+ * @since 4.0
+ */
+public class RgParserFactory {
+
+   public Ispn5_2CsvParser buildParser(String path) throws Exception {
+      Ispn5_2CsvParser p = new Ispn5_2CsvParser(path);
+      if (p.getReplicationProtocol().equals("PB"))
+         return new Ispn5_2CsvParser_PB(path);
+      return p;
+   }
+}
---
+++ src/main/java/parse/radargun/Ispn5_2CsvParser_PB.java
@@ -0,0 +1,17 @@
+package parse.radargun;
+
+import java.io.IOException;
+
+/**
+ * // TODO: Document this
+ *
+ * @author diego
+ * @since 4.0
+ */
+public class Ispn5_2CsvParser_PB extends Ispn5_2CsvParser {
+
+   public Ispn5_2CsvParser_PB(String path) throws IOException {
+      super(path);
+   }
+
+}
